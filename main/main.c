68a55ac9f5792d0c97de7895032ee8b598409b63
Sebastian Ohl <sebastian@ohl.de>
2021-03-19 08:24:50 +0100

    add ota

--- main/main.c
+++ main/main.c
@@ -3,6 +3,7 @@
 #include "esp_event_loop.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
+#include "esp_ota_ops.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 #include "freertos/task.h"
@@ -10,6 +11,7 @@
 #include "rom/ets_sys.h"
 #include <string.h>
 
+#include "ota.h"
 #include "mqtt_client.h"
 #include "sentio.h"
 
@@ -24,8 +26,13 @@ static EventGroupHandle_t wifi_event_group;
 static EventGroupHandle_t mqtt_event_group;
 
 static esp_mqtt_client_handle_t mqtt_client = NULL;
+static int OTA_ongoing = HOMIE_FALSE;
 
-const static int MQTT_CONNECTED_BIT = BIT0;
+const static int MQTT_NEW_CONNECT_BIT = BIT0;
+const static int MQTT_CONNECTED_BIT = BIT1;
+
+void start_ota(struct homie_handle_s *handle, int node,
+                                int property, const char *data, int data_len);
 
 homie_handle_t homie = {
     .deviceid = "sentio-b2-control",
@@ -40,7 +47,7 @@ homie_handle_t homie = {
     .nodes = {{.id = "sentio-b2-control",
                .name = "Sentio B2 control",
                .type = "B2",
-               .num_properties = 8,
+               .num_properties = 9,
                .properties =
                    {
                        {
@@ -131,6 +138,17 @@ homie_handle_t homie = {
                            .write_property_cbk = &write_heattimer_value,
                            .user_data = NULL,
                        },
+                       {
+                           .id = "update",
+                           .name = "update",
+                           .settable = HOMIE_TRUE,
+                           .retained = HOMIE_TRUE,
+                           .unit = " ",
+                           .datatype = HOMIE_BOOL,
+                           .read_property_cbk = NULL,
+                           .write_property_cbk = &start_ota,
+                           .user_data = NULL,
+                       },
                    }}},
     .uptime = 0,
 };
@@ -145,10 +163,12 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
         xEventGroupSetBits(mqtt_event_group, MQTT_CONNECTED_BIT);
+        xEventGroupSetBits(mqtt_event_group, MQTT_NEW_CONNECT_BIT);
         break;
     case MQTT_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
         xEventGroupClearBits(mqtt_event_group, MQTT_CONNECTED_BIT);
+        xEventGroupClearBits(mqtt_event_group, MQTT_NEW_CONNECT_BIT);
         break;
 
     case MQTT_EVENT_SUBSCRIBED:
@@ -206,8 +226,17 @@ static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
 
     switch (event->event_id)
     {
-    case SYSTEM_EVENT_STA_START:
-        esp_wifi_connect();
+    case SYSTEM_EVENT_STA_START: {
+            esp_wifi_connect();
+            esp_err_t err;
+            char hostname[33] = {0};
+            snprintf(hostname, 33, "sentio-b2");
+            ESP_LOGI(TAG, "set hostname to %s", hostname);
+            if ((err = tcpip_adapter_set_hostname(WIFI_IF_STA, hostname)) != ESP_OK)
+            {
+                ESP_LOGE(TAG, "set hostname failed: %s", esp_err_to_name(err));
+            }
+        }
         break;
     case SYSTEM_EVENT_STA_GOT_IP:
         ESP_LOGI(TAG, "got ip:%s",
@@ -275,7 +304,6 @@ void connect_to_wifi()
     };
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
 
     uint8_t eth_mac[6];
     esp_wifi_get_mac(WIFI_IF_STA, eth_mac);
@@ -283,9 +311,41 @@ void connect_to_wifi()
              eth_mac[0], eth_mac[1], eth_mac[2], eth_mac[3], eth_mac[4],
              eth_mac[5]);
 
+    ESP_LOGI(TAG, "homie device id %s", homie.deviceid);
     ESP_LOGI(TAG, "wifi_init_sta finished.");
     ESP_LOGI(TAG, "connect to ap SSID:%s password:%s", CONFIG_WIFI_SSID,
              CONFIG_WIFI_PASSWORD);
+
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+void start_ota(struct homie_handle_s *handle, int node, int property, const char *data, int data_len)
+{
+    if (data_len > 0)
+    {
+        char buf_topic[255] = {0};
+        char url[255] = {0};
+
+        snprintf(url, sizeof(url), "%.*s", data_len, data);
+        
+        OTA_ongoing = HOMIE_TRUE;
+        ESP_LOGI(TAG, "get OTA update from %s", url);
+
+        esp_err_t ret = execute_ota(url);
+
+        // reset update topic to prevent inifinte update loop
+        sprintf(buf_topic, "homie/%s/%s/update/set", homie.deviceid, homie.nodes[0].id);
+        esp_mqtt_client_publish(homie.mqtt_client, buf_topic, "",
+                            0, 1, HOMIE_TRUE);
+        if (ret == ESP_OK)
+        {
+            ESP_LOGI(TAG, "reset to start new image");
+            fflush(stdout);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            esp_restart();
+        }
+        OTA_ongoing = HOMIE_FALSE;
+    }
 }
 
 void app_main(void)
@@ -305,6 +365,12 @@ void app_main(void)
 
     connect_to_wifi();
 
+    const esp_partition_t* current_partition = esp_ota_get_running_partition();
+    strncpy(homie.firmware, current_partition->label, sizeof(homie.firmware));
+
+    const esp_app_desc_t *app_desc = esp_ota_get_app_description();
+    strncpy(homie.firmware_version, app_desc->version, sizeof(homie.firmware_version));
+    ESP_LOGI(TAG, "running partition %s version %s", current_partition->label, app_desc->version);
     mqtt_app_start();
     homie.mqtt_client = mqtt_client;
 
@@ -316,18 +382,25 @@ void app_main(void)
         EventBits_t uxBits;
         ESP_LOGI(TAG, "Restarting in %d seconds...\n", i * 5);
 
+        homie.uptime += 30;
+
         ESP_LOGI(TAG, "test for mqtt new connect");
-        uxBits = xEventGroupWaitBits(mqtt_event_group, MQTT_CONNECTED_BIT, true,
-                                     true, 1);
-        if ((uxBits & MQTT_CONNECTED_BIT) != 0)
+        uxBits = xEventGroupWaitBits(mqtt_event_group, MQTT_NEW_CONNECT_BIT, true,
+                                     false, 1);
+        if ((uxBits & MQTT_NEW_CONNECT_BIT) != 0)
         {
             ESP_LOGI(TAG, "homie init");
             homie_init(&homie);
             ESP_LOGI(TAG, "homie init done");
         }
-
-        homie.uptime += 30;
-        homie_cycle(&homie);
+        
+        uxBits = xEventGroupWaitBits(mqtt_event_group, MQTT_CONNECTED_BIT, false,
+                                     false, 1);
+        if ((uxBits & MQTT_CONNECTED_BIT) != 0)
+        {
+            ESP_LOGI(TAG, "homie cycle");
+            homie_cycle(&homie);
+        }
 
         uart_cycle(&uart); // is waiting 30 sec
     }
